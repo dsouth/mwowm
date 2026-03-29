f409acd237ce7105035050abfef2eca99a34fd23
Doug South <doug.south@gmail.com>
2026-03-29 16:19:31 +1000

    remove log messages

--- src/cursor.c
+++ src/cursor.c
@@ -10,11 +10,7 @@ void cursor_motion(struct wl_listener *listener, void *data) {
 	wlr_log(WLR_DEBUG, "cursor motion called");
 	struct window_manager *wm = wl_container_of(listener, wm, cursor_motion_listener);
 	struct wlr_pointer_motion_event *event = data;
-
 	wlr_cursor_move(wm->cursor, &event->pointer->base, event->delta_x, event->delta_y);
-	wlr_log(WLR_DEBUG, "%lf, %lf event", event->delta_x, event->delta_y);
-	wlr_log(WLR_DEBUG, "%lf, %lf cursor", wm->cursor->x, wm->cursor->y);
-
 	wlr_cursor_set_xcursor(wm->cursor, wm->cursor_manager, "default");
 }
 
